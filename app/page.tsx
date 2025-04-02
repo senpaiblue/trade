e25cbda5a22c9dd0d1bd723c830c6e1f068869ca
Sakcham Singh <sakchamsingh@Sakchams-Mac-mini.local>
2025-04-02 20:54:27 +0530

    new components added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Navbar from "@/components/Navbar";
 import Home from "../components/Home";
+import HorizontalScrollCarousel from "@/components/FeaturesCarousels";
+import ColorChangeCards from "@/components/FeatureCards";
+import BasicFAQ from "@/components/Faq";
 
 export default function main() {
   return (
     <div className=" py-8">
       <Navbar/>
       <Home />
-      <Home />
-      <Home />
+      <HorizontalScrollCarousel />
+      <ColorChangeCards />
+      <BasicFAQ />
     </div>
   );
 }
